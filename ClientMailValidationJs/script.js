3610155eae2327fbeb3d5ef26c271b5df9a15320
JKAravind <aruvi.suvel@gmail.com>
2025-03-17 23:03:09 +0530

    final Client

--- ClientMailValidationJs/script.js
+++ ClientMailValidationJs/script.js
@@ -1,10 +1,15 @@
 regex = /^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-z]{2,}$/;
+errorMessage = document.getElementById("errorMessage");
+
 function clientValidate(){
     let userMail = document.getElementById("emailBox").value;
     if(!(regex.test(userMail))){
-        console.log("invalid");
+        errorMessage.innerHTML = "Enter a Valid Email Id";
+        errorMessage.style.color = "red";
     }
     else{
+        errorMessage.innerHTML = "Valid";
+        errorMessage.style.color = "green";
         console.log("valid")
     }
 }
