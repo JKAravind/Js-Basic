ac06dec6ce734fe2e6ea1ccd6921b6867120dd10
JKAravind <aruvi.suvel@gmail.com>
2025-03-21 00:36:54 +0530

    check box done need to add css when user complete task

--- TodoList/script.js
+++ TodoList/script.js
@@ -3,7 +3,6 @@ function addTask(){
     task = document.getElementById("enterTask").value
 
 
-
     taskCell = document.createElement("div")
     taskCell.setAttribute("class","taskCell")
 
@@ -13,13 +12,23 @@ function addTask(){
 
     checkBox = document.createElement("input")
     checkBox.type = "checkbox"
+    checkBox.name = "checkbox"
     taskCell.appendChild(checkBox)
-
-
-
-
-
 
     document.getElementById("task-container").appendChild(taskCell)
 
+
 }
+
+var taskcontainer = document.getElementById("task-container")
+taskcontainer.addEventListener("change" , function(event){
+    if (event.target.matches("input[name=checkbox]")){
+        console.log("jello")
+        if(event.target.checked){
+            console.log("checked")
+        }
+        else{
+            console.log("not")
+        }
+    }
+})
